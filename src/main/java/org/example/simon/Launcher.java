a0cf4a7644c4cc4dc3c2519a39c508b304493dc9
Evelyn Cassinotte <e.cass03@gmail.com>
2024-01-01 22:23:49 -0300

    add: exe file

---
+++ src/main/java/org/example/simon/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.simon;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(org.example.simon.Application.class, args);
+    }
+}
